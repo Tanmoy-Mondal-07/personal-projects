0077c9fc0bfc6ce5837bb03bf75ed9b3ad9bde5c
Tanmay Mondal <tanmoy10122000@gmail.com>
2025-11-29 20:29:08 +0530

    ..

--- markscalculator/astro.config.mjs
+++ markscalculator/astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import cloudflare from "@astrojs/cloudflare";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  adapter: cloudflare(),
-  integrations: [react()]
+  site: "https://personal-projects-djf.pages.dev",
+  integrations: [react(),sitemap()]
 });
